370f3f264bc8535fa35547c89da658549351497b
jayrgo <jayrgo@users.noreply.github.com>
2020-02-21 21:48:48 +0100

    check for spells that consumes the whole power

---
+++ .luacheckrc
@@ -0,0 +1,14 @@
+-- luacheck: ignore
+std = "lua51"
+max_line_length = false
+exclude_files = {"Libs/", ".luacheckrc"}
+ignore = {
+    "211", -- Unused local variable
+    "212", -- Unused argument
+}
+globals = {
+    "CreateFrame", "LibStub", "UnitPower", "UnitPowerMax", "GetSpellPowerCost", "GetActionInfo", "GetMacroSpell",
+    "GetSpellCooldown", "GetTime", "IsUsableAction", "ActionButton_CalculateAction", "Enum", "UnitPowerType", "C_Timer",
+    "format", "tContains", "wipe", "hooksecurefunc", "Dominos", "BOOKTYPE_SPELL", "GetActionTexture", "GetMacroInfo",
+    "GetSpellBookItemName", "GetSpellBookItemTexture", "GetAddOnInfo", "GetAddOnMetadata", "GetLocale",
+}
--- CountIt.lua
+++ CountIt.lua
@@ -19,20 +19,14 @@ local LSM = LibStub("LibSharedMedia-3.0")
 local function updateText(button)
     if button then
         local fontString = fontStrings[button]
-        fontString:SetDrawLayer(
-            DB:Get("visibility", "mouseOver") and "HIGHLIGHT" or "OVERLAY")
+        fontString:SetDrawLayer(DB:Get("visibility", "mouseOver") and "HIGHLIGHT" or "OVERLAY")
         fontString:ClearAllPoints()
-        fontString:SetPoint(DB:Get("position", "point"),
-                            DB:Get("position", "ofsx"), DB:Get("position", "ofsy"))
+        fontString:SetPoint(DB:Get("position", "point"), DB:Get("position", "ofsx"), DB:Get("position", "ofsy"))
         fontString:SetScale(DB:Get("appearance", "scale"))
-        fontString:SetFont(LSM:Fetch(LSM.MediaType.FONT,
-                                     DB:Get("appearance", "font")),
-                           DB:Get("appearance", "height"),
+        fontString:SetFont(LSM:Fetch(LSM.MediaType.FONT, DB:Get("appearance", "font")), DB:Get("appearance", "height"),
                            DB:Get("appearance", "outline") and "OUTLINE" or "")
-        fontString:SetTextColor(DB:Get("appearance", "color", "r"),
-                                DB:Get("appearance", "color", "g"),
-                                DB:Get("appearance", "color", "b"),
-                                DB:Get("appearance", "color", "a"))
+        fontString:SetTextColor(DB:Get("appearance", "color", "r"), DB:Get("appearance", "color", "g"),
+                                DB:Get("appearance", "color", "b"), DB:Get("appearance", "color", "a"))
         if DB:Get("appearance", "shadow") then
             fontString:SetShadowOffset(1, -1)
         else
@@ -49,7 +43,7 @@ local power = setmetatable({}, {
         local power = UnitPower("player", k)
         t[k] = power
         return power
-    end
+    end,
 })
 local UnitPowerMax = UnitPowerMax
 local maxPower = setmetatable({}, {
@@ -57,8 +51,56 @@ local maxPower = setmetatable({}, {
         local maxpower = UnitPowerMax("player", k)
         t[k] = maxpower
         return maxpower
-    end
+    end,
 })
+
+local CONSUMES_ALL = {
+    -- Execute
+    [5308] = true, -- Rank 1
+    [20658] = true, -- Rank 2
+    [20660] = true, -- Rank 3
+    [20661] = true, -- Rank 4
+    [20662] = true, -- Rank 5
+    -- Eviscerate
+    [2098] = true, -- Rank 1
+    [6760] = true, -- Rank 2
+    [6761] = true, -- Rank 3
+    [6762] = true, -- Rank 4
+    [8623] = true, -- Rank 5
+    [8624] = true, -- Rank 6
+    [11299] = true, -- Rank 7
+    [11300] = true, -- Rank 8
+    [31016] = true, -- Rank 9
+    -- Slice and Dice
+    [5171] = true, -- Rank 1
+    [6774] = true, -- Rank 2
+    -- Expose Armor
+    [8647] = true, -- Rank 1
+    [8649] = true, -- Rank 2
+    [8650] = true, -- Rank 3
+    [11197] = true, -- Rank 4
+    [11198] = true, -- Rank 5
+    -- Rupture
+    [1943] = true, -- Rank 1
+    [8639] = true, -- Rank 2
+    [8640] = true, -- Rank 3
+    [11273] = true, -- Rank 4
+    [11274] = true, -- Rank 5
+    [11275] = true, -- Rank 6
+    -- Rip
+    [1079] = true, -- Rank 1
+    [9492] = true, -- Rank 2
+    [9493] = true, -- Rank 3
+    [9752] = true, -- Rank 4
+    [9894] = true, -- Rank 5
+    [9896] = true, -- Rank 6
+    -- Ferocious Bite
+    [22568] = true, -- Rank 1
+    [22827] = true, -- Rank 2
+    [22828] = true, -- Rank 3
+    [22829] = true, -- Rank 4
+    [31018] = true, -- Rank 5
+}
 
 local GetSpellPowerCost = GetSpellPowerCost
 local spellPowerCost = setmetatable({}, {
@@ -66,7 +108,7 @@ local spellPowerCost = setmetatable({}, {
         local v = GetSpellPowerCost(k)
         t[k] = v
         return v
-    end
+    end,
 })
 
 local floor = math.floor
@@ -78,6 +120,7 @@ local GetTime = GetTime
 local IsUsableAction = IsUsableAction
 ---@param action number
 ---@return number count
+---@return number maxCount
 local function getActionCount(action)
     if not action then return 0, 0 end
     if not IsUsableAction(action) then return 0, 0 end
@@ -92,10 +135,7 @@ local function getActionCount(action)
     if DB:Get("visibility", "cooldown", "hide") then
         local start, duration, enabled, modRate = GetSpellCooldown(id)
         if (start > 0 and duration > 0) and
-            ((start + duration) > GetTime() +
-                DB:Get("visibility", "cooldown", "threshold")) then
-            return -1, 0
-        end
+            ((start + duration) > GetTime() + DB:Get("visibility", "cooldown", "threshold")) then return -1, 0 end
     end
     local count = 0
     local maxCount = 0
@@ -108,6 +148,10 @@ local function getActionCount(action)
                 maxCount = max(maxCount, maxPower[cost.type] / cost.cost)
             end
         end
+    end
+    if CONSUMES_ALL[id] then
+        count = count > 1 and 1 or count
+        maxCount = maxCount > 1 and 1 or maxCount
     end
     return floor(count), floor(maxCount)
 end
@@ -201,7 +245,7 @@ do -- getPowerTypeFromToken
         FURY = Enum.PowerType.Fury,
         PAIN = Enum.PowerType.Pain,
         INSANITY = Enum.PowerType.Insanity,
-        COMBO_POINTS = Enum.PowerType.ComboPoints
+        COMBO_POINTS = Enum.PowerType.ComboPoints,
     }
     ---@param powerToken string
     ---@return number powerType
@@ -210,19 +254,16 @@ end
 
 LibStub("LibJayEvent"):Register("ACTIONBAR_UPDATE_STATE", updateAction)
 
-LibStub("LibJayEvent"):Register("ACTIONBAR_SLOT_CHANGED",
-                                function() updateCount() end)
+LibStub("LibJayEvent"):Register("ACTIONBAR_SLOT_CHANGED", function() updateCount() end)
 
 LibStub("LibJayEvent"):Register("ACTIONBAR_UPDATE_USABLE", updateCount)
 
 LibStub("LibJayEvent"):Register("ACTIONBAR_UPDATE_COOLDOWN", updateCount)
 
 local Wait = C_Timer.After
-LibStub("LibJayEvent"):Register("ACTIONBAR_PAGE_CHANGED",
-                                function() Wait(0.1, updateAction) end)
+LibStub("LibJayEvent"):Register("ACTIONBAR_PAGE_CHANGED", function() Wait(0.1, updateAction) end)
 
-LibStub("LibJayEvent"):Register("UNIT_POWER_FREQUENT",
-                                function(unitTarget, powerToken)
+LibStub("LibJayEvent"):Register("UNIT_POWER_FREQUENT", function(unitTarget, powerToken)
     if unitTarget == "player" then
         local powerType = getPowerTypeFromToken(powerToken)
         power[powerType] = UnitPower(unitTarget, powerType)
@@ -232,8 +273,7 @@ end)
 
 LibStub("LibJayEvent"):Register("UNIT_MAXPOWER", function(unitTarget, powerType)
     if unitTarget == "player" then
-        maxPower[powerType] = UnitPowerMax(unitTarget,
-                                           getPowerTypeFromToken(powerType))
+        maxPower[powerType] = UnitPowerMax(unitTarget, getPowerTypeFromToken(powerType))
         updateCount()
     end
 end)
@@ -245,20 +285,17 @@ local function updateSpellPowerCost()
 end
 
 local UnitPowerType = UnitPowerType
-LibStub("LibJayEvent"):Register("PLAYER_ENTERING_WORLD",
-                                function(isInitialLogin, isReloadingUi)
+LibStub("LibJayEvent"):Register("PLAYER_ENTERING_WORLD", function(isInitialLogin, isReloadingUi)
     local powerType = UnitPowerType("player")
     power[powerType] = UnitPower("player", powerType)
     maxPower[powerType] = UnitPowerMax("player", powerType)
     updateAction()
 end)
 
-LibStub("LibJayEvent"):Register("UNIT_DISPLAYPOWER",
-                                function() updateCount() end)
+LibStub("LibJayEvent"):Register("UNIT_DISPLAYPOWER", function() updateCount() end)
 
-LibStub("LibJayEvent"):Register("UNIT_AURA", function(unitTarget)
-    if unitTarget == "player" then updateSpellPowerCost() end
-end)
+LibStub("LibJayEvent"):Register("UNIT_AURA",
+                                function(unitTarget) if unitTarget == "player" then updateSpellPowerCost() end end)
 LibStub("LibJayEvent"):Register("CHARACTER_POINTS_CHANGED", updateSpellPowerCost)
 LibStub("LibJayEvent"):Register("SPELLS_CHANGED", updateSpellPowerCost)
 
@@ -271,9 +308,7 @@ local type = type
 ---@param button table
 function AddOn.RegisterButton(button)
     if type(button) ~= "table" then
-        error(format(
-                  "Usage: %s:RegisterButton(button): 'button' - table expected got %s",
-                  AddOnName, type(button)), 2)
+        error(format("Usage: %s:RegisterButton(button): 'button' - table expected got %s", AddOnName, type(button)), 2)
     end
 
     if tContains(buttons, button) then return end
